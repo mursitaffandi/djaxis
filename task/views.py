ce62d6dc38aedf352c1c63139657d5f677add1d5
mursitaffandi <mursitaffandi@gmail.com>
2021-08-04 17:09:35 +0700

    formuliring

--- task/views.py
+++ task/views.py
@@ -3,4 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(req):
+	if req.POST:
+		username = req.POST['username']
+		print(username)
 	return render(req, 'index.html')
+
+
+def about(req):
+	return render(req, 'about.html')
